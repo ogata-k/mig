21b3d2ee0fdbb1c0a24010d82e94cb055fcd38d3
ogata-k <ogtkzk712@gmail.com>
2019-04-29 09:58:49 +0900

    delete Provisional implementation messages

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -60,7 +60,7 @@ pub fn action_controller(matches: ArgMatches) -> Result<&str, &str> {
     }
     // input_file_opt is not none!!!
     let input_file_opt_str = input_file_opt.unwrap();
-    let input_file_path = PathBuf::from(input_file_opt_str); // TODO now, Provisional implementation
+    let input_file_path = PathBuf::from(input_file_opt_str);
 
     let framework_opt = matches.value_of("TARGET_FW");
     if framework_opt.is_none() {
@@ -77,7 +77,7 @@ pub fn action_controller(matches: ArgMatches) -> Result<&str, &str> {
         return Err("out put file is not specified. Why?");
     }
     let output_file_path_string = with_timestamp(&output_file_opt.unwrap(), &framework_type.unwrap());
-    let output_file_path = PathBuf::from(output_file_path_string.as_str()); // TODO now, Provisional implementation
+    let output_file_path = PathBuf::from(output_file_path_string.as_str());
 
     // TODO input/output files existing and isFile and extension check
 
