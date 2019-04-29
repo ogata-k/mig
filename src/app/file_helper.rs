d99738a640a3846e8d09f147e7b49518b7db60a1
ogata-k <ogtkzk712@gmail.com>
2019-04-29 09:21:39 +0900

    make helper for timestamp generator

--- src/app/file_helper.rs
+++ src/app/file_helper.rs
@@ -1,8 +1,19 @@
+extern crate chrono;
+
 use std::path::PathBuf;
+
+use chrono::Local;
 
 use crate::app::framework::Framework;
 
 pub fn with_timestamp<'a>(target_file: &'a PathBuf, framework_type: &Framework) -> &'a PathBuf {
     // TODO set timestamp header to target file
     return target_file;
+}
+
+fn generate_timestamp(framework_type: &Framework) -> String {
+    let fmt = match framework_type {
+        Framework::Laravel => "%Y_%m_%d %H%M%S",
+    };
+    return Local::now().format(fmt).to_string();
 }
