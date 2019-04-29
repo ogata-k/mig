104c15f7ff9b51cb7b6b6163767197d68e9e605c
ogata-k <ogtkzk712@gmail.com>
2019-04-29 16:46:25 +0900

    define tokens

---
+++ src/app/converter/token.rs
@@ -0,0 +1,19 @@
+#[derive(Eq)]
+pub enum Token {
+    LMidParen,
+    RMidParen,
+    Name(String),
+    // table, column
+    NameColon(String),
+    // command, opt
+    Ymd(u16, u8, u8),
+    Time(u8, u8, u8),
+    DateTime(u16, u8, u8, u8, u8, u8),
+    Integer(i16),
+    Double(f32),
+    String(String),  // "..."
+}
+
+pub struct Sequence {
+    pub seq: Vec<Token>
+}
