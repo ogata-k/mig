08b952a305e69c74730e0324f5b3791379201d6f
ogata-k <ogtkzk712@gmail.com>
2019-04-29 09:30:22 +0900

    to visible easily

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -59,7 +59,8 @@ pub fn action_controller(matches: ArgMatches) -> Result<&str, &str> {
         return Err("input file is not specified. Why?");
     }
     // input_file_opt is not none!!!
-    let input_file_path = PathBuf::from(input_file_opt.unwrap()); // TODO now, Provisional implementation
+    let input_file_opt_str = input_file_opt.unwrap();
+    let input_file_path = PathBuf::from(input_file_opt_str); // TODO now, Provisional implementation
 
     let framework_opt = matches.value_of("TARGET_FW");
     if framework_opt.is_none() {
@@ -76,8 +77,8 @@ pub fn action_controller(matches: ArgMatches) -> Result<&str, &str> {
     if output_file_opt.is_none() {
         return Err("out put file is not specified. Why?");
     }
-    let output_file_path_ = with_timestamp(&output_file_opt.unwrap(), &framework_type.unwrap());
-    let output_file_path = PathBuf::from(output_file_path_); // TODO now, Provisional implementation
+    let output_file_path_str = with_timestamp(&output_file_opt.unwrap(), &framework_type.unwrap());
+    let output_file_path = PathBuf::from(output_file_path_str); // TODO now, Provisional implementation
 
     // TODO input/output files existing and isFile check
 
