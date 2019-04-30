6fa3e97a27c621d92a424ded61145ebcd2c0f45e
ogata-k <ogtkzk712@gmail.com>
2019-04-30 09:31:50 +0900

    make taking error

--- src/app/converter/mod.rs
+++ src/app/converter/mod.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use crate::app::converter::parser::lexical_analyzer;
 use crate::app::framework::Framework;
 
 mod token;
@@ -12,6 +13,12 @@ pub fn convert_to_migration_file(
 ) -> Result<&'static str, &'static str> {
     // TODO convert from input file "input" to output file "output" with target framework "framework"
     // this function is controller for convert
+
+    let token_seq_result = lexical_analyzer("");
+    if token_seq_result.is_err() {
+        return Err(&token_seq_result.err().unwrap());
+    }
+    let token_seq = token_seq_result.unwrap();
     // return Ok("Success converted");
     return Err("failed convert");
 }
