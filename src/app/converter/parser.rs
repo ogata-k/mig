5e7bde2951b58c7108870d66599c7dd7f3423629
ogata-k <ogtkzk712@gmail.com>
2019-04-30 11:24:15 +0900

    starting implement's test

--- src/app/converter/parser.rs
+++ src/app/converter/parser.rs
@@ -1,16 +1,9 @@
-extern crate combine;
-
-use combine::Parser;
-use combine::parser::char::digit;
-
 use crate::app::converter::token::{Sequence, Token};
-
-use self::combine::token;
 
 pub fn lexical_analyzer(input: String) -> Result<Sequence, &'static str> {
     let mut seq: Vec<Token> = vec!();
 
     // TODO use parsers
-
+    // https://qiita.com/agatan/items/8a097ead46df1c1659ff
     return Ok(Sequence::from(seq));
 }
