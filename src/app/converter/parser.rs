d9a37020212dcbd86700180866f049c494582216
ogata-k <ogtkzk712@gmail.com>
2019-05-01 10:44:51 +0900

    counter bug fix

--- src/app/converter/parser.rs
+++ src/app/converter/parser.rs
@@ -16,7 +16,7 @@ pub struct Counter {
 
 impl Counter {
     pub fn new() -> Self {
-        return Counter { cursor: (1, 1), position: 1, is_new_line: false };
+        return Counter { cursor: (0, 0), position: 0, is_new_line: true };
     }
 }
 
@@ -32,6 +32,12 @@ impl<'a> Stream<'a> {
     }
 
     fn next(&mut self) -> Result<char, &str> {
+        if self.counter.is_new_line {
+            // if before char is \n, now reading char is head of newline
+            self.counter.cursor.0 += 1;
+            self.counter.cursor.1 = 0;
+            self.counter.is_new_line = false;
+        }
         let ch_opt = self.chars.next();
         if ch_opt.is_none() {
             if self.chars.as_str() == "" {
@@ -44,18 +50,17 @@ impl<'a> Stream<'a> {
             return Err("next char is not ascii character")
         }
 
-        self.counter.position += 1;
-        if self.counter.is_new_line {
-            // if before char is \n, now reading char is head of newline
-            self.counter.cursor.0 += 1;
-            self.counter.cursor.1 = 0;
-            self.counter.is_new_line = false;
-        }
         // update is next line
+        if ch == '\r' {  // for windows
+            self.counter.position += 1;
+            return self.next();
+        }
         if ch == '\n' {
             self.counter.is_new_line = true;
         }
-        // update column counter
+
+        // update counter
+        self.counter.position += 1;
         self.counter.cursor.1 += 1;
         return Ok(ch);
     }
