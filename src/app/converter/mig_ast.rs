711492fcffd12c9049e57174b405c97caf0d8f5e
ogata-k <ogtkzk712@gmail.com>
2019-05-15 21:18:48 +0900

    modify AST

--- src/app/converter/mig_ast.rs
+++ src/app/converter/mig_ast.rs
@@ -2,21 +2,21 @@ use std::collections::{HashMap, HashSet};
 
 use crate::app::converter::token::Token;
 
-/// AST
 #[derive(Debug, Clone, PartialEq)]
 pub enum Ast {
-    Method { method: String, table_name: String, table_define: Box<Ast> },
+    Program { start: Box<Ast> },
+    Method { method: Box<Ast>, table_name: Box<Ast>, table_define: Box<Ast> },
     // HashMap<option_name, options>
     Options { table_define: HashMap<String, Vec<Box<Ast>>> },
-    // column and table options
-    Option { option_type: OptionType, option_name: String, option_param: Vec<Ast> },
-    Token(Token),
+    ColumnOption { option_name: Box<Ast>, option_param: Vec<Box<Ast>> },
+    TableOption { option_name: String, option_param: Vec<Box<Ast>> },
+    Ymd(Box<Ast>, Box<Ast>, Box<Ast>),
+    Time(Box<Ast>, Box<Ast>, Box<Ast>),
+    DateTime(Box<Ast>, Box<Ast>, Box<Ast>, Box<Ast>, Box<Ast>, Box<Ast>),
+    Integer(i16),
+    Double(f32),
+    String(String),
 }
 // TODO SequenceからAstへの変換とAstの最適化(結果はMig構造体という名前に）した木への変換を実装する。
 // TODO 最終的に木を走査しながら文字列へ変換する
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum OptionType {
-    Column,
-    Table,
-}
